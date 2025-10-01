0e72adea5c4e2e637a4c901cde8a8a9e4ebbb32f
hoangtung <luuhoangtung2710@gmail.com>
2025-10-01 16:05:36 +0700

    Thêm toàn bộ code

---
+++ HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+
+        string hoTen = "Lưu Hoàng Tùng";
+        string maSV = "12424034";
+        string lop = "124241";
+        string github = "hoangtung-123";
+        string email = "luuhoangtung2710@gmail.com";
+
+
+        Console.WriteLine(hoTen + "\t" + maSV + "\t" + lop + "\t" + github + "\t" + email);
+
+
+        Console.ReadLine();
+    }
+}
